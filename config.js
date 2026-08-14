6f1ad23a5ba4d580689fa54d375d2d6bc96b5530
rhtampubolon-creator <rh.tampubolon@gmail.com>
2026-08-14 13:16:48 +0700

    Fix early BidderList bridge loader

--- config.js
+++ config.js
@@ -28,8 +28,9 @@ window.APP_CONFIG = Object.freeze({
   // sengaja di fase parser agar bridge lokal aktif sinkron SEBELUM script besar
   // BidderList menjalankan initializeBidderWorkspace() dan preload master lama.
   if (document.readyState === "loading") {
-    const bridgeUrl = new URL("./local-pr-bridge.js", window.location.href).href + "?v=20260814-local-pr-v1";
-    document.write('<script data-msw-bidder-local-pr-bridge="true" src="' + bridgeUrl.replace(/&/g, '&amp;').replace(/"/g, '&quot;') + '"><\\/script>');
+    const bridgeUrl = new URL("./local-pr-bridge.js", window.location.href).href + "?v=20260814-local-pr-v2";
+    const safeUrl = bridgeUrl.replace(/&/g, "&amp;").replace(/"/g, "&quot;");
+    document.write('<script data-msw-bidder-local-pr-bridge="true" src="' + safeUrl + '"></' + 'script>');
   }
 })();
 
