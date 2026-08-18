40311e672e17a7e3b5df47dac299a569705b0ac9
rhtampubolon-creator <rh.tampubolon@gmail.com>
2026-08-18 10:52:38 +0700

    Bump Procurement cache for Buyer-scoped All Clear

--- shared-modules.js
+++ shared-modules.js
@@ -12,7 +12,7 @@
       navId: "workspaceProcurement",
       label: "Procurement",
       icon: "clipboard-list",
-      url: "procurement-admin/index.html?v=20260818-allclear-cache-h1",
+      url: "procurement-admin/index.html?v=20260818-buyer-allclear-v355",
       roles: ["SUPER_ADMIN", "BUYER"]
     },
     {
